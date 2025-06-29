1d473004dab72e8850b68b2bcf1d317c588a8fa8
thomasgmg <cabralzinho.mg@gmail.com>
2025-06-29 18:33:40 +0200

    update window gaps

--- config.h
+++ config.h
@@ -8,17 +8,17 @@
  #define TERMINAL "ghostty"
 
 /* appearance */
-static const unsigned int borderpx = 5; /* border pixel of windows */
+static const unsigned int borderpx = 2; /* border pixel of windows */
 static const unsigned int barpadv	= 7;		/* Vertical padding: how far the bar is from the top */
 static const unsigned int barpadh	= 15;		/* Horizontal padding: how far the bar is from each side */
-static const unsigned int barheight	= 3;		/* Bar height: How long the bar is vertically */
+static const unsigned int barheight	= 2;		/* Bar height: How long the bar is vertically */
 static const unsigned int barborder	= 3;		/* bar vertical padding (from top)*/
 static const unsigned int floatbar	= 1;		/* 0 means bar won't float; float or dock the bar */
 static const unsigned int snap = 32;    /* snap pixel */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappov    = 15;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
