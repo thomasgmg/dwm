dcce4415ee214a2e474b6006c6adf9bb698ab12a
TMG <thomas@voidthomas.lan>
2025-08-27 10:14:28 +0000

    change barpadding

--- config.h
+++ config.h
@@ -15,8 +15,8 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 #define ICONSIZE bh   /* icon size */
 #define ICONSPACING (bh -4) /* space between icon and title */
-static const int vertpad            = 5;        /* vertical padding of bar */
-static const int sidepad            = 5;       /* horizontal padding of bar */
+static const int vertpad            = 10;        /* vertical padding of bar */
+static const int sidepad            = 10;       /* horizontal padding of bar */
 static const char *fonts[]          = { "monospace:size=11" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
